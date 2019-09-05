310ed321a8fbc1ac62f79f71039dee28ae1059c1
billy <billyliadphillips@gmail.com>
2019-09-05 13:47:25 +0100

    updated routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const messages = [
+  {
+    user: "Timmy",
+    text: "Woah, hello world!",
+    added: new Date()
+  },
+  {
+    user: "Matt",
+    text: "Surfs up, dude!",
+    added: new Date()
+  },
+  {
+    user: "Andrew",
+    text: "Cool story, bro!",
+    added: new Date()
+  }
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Mini Message Board", messages: messages });
 });
 
 module.exports = router;
